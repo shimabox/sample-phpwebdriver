db5b78ce189fd150b405dad964338066aa690307
shimabox <shimabox@gmail.com>
2017-07-27 01:12:35 +0900

    [modify] kill Xvfb and geckodriver

--- kill_selenium.sh
+++ kill_selenium.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 ps aux | grep [s]elenium-server-standalone | awk '{ print "kill -9", $2 }' | sh
+ps aux | grep [X]vfb | awk '{ print "sudo kill -9", $2 }' | sh
+ps aux | grep [g]eckodriver | awk '{ print "kill -9", $2 }' | sh
