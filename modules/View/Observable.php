4c99b71d1be99ebe3ca9018f9c3016c1f2c08934
shimabox <shimabox@gmail.com>
2019-06-19 06:36:14 +0900

    スティッキー(固定)ヘッダーなどの表示をコントロールしたかったので画面描画の切り替えタイミングを通知するためのObserverを作成

---
+++ modules/View/Observable.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace SMB\PhpWebDriver\Modules\View;
+
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+
+/**
+ * Interface of Observer
+ * 
+ * |   view    |<br>
+ * |1.|2.|2.|3.|<br>
+ * |4.|2.|2.|2.|<br>
+ * |5.|2.|2.|6.|
+ * 
+ * |1.|<br>
+ * |4.|<br>
+ * |2.|<br>
+ * |6.|
+ * 
+ * Notification priority.<br>
+ * 1. > 6. > 3. > 4. > 5. > 2.
+ * 
+ */
+interface Observable
+{
+    /**
+     * 1. はじめて画面が描画されることを通知
+     * Notify that the first view is drawn.
+     * 
+     * @param RemoteWebDriver $driver
+     * @param int $contentsWidth  実際のコンテンツ横幅
+     * @param int $contentsHeight 実際のコンテンツ縦幅
+     * @param int $scrolledWidth  現在スクロール済みの横幅
+     * @param int $scrolledHeight 現在スクロール済みの縦幅
+     */
+    public function notifyFirstRender(
+        RemoteWebDriver $driver,
+        $contentsWidth,
+        $contentsHeight,
+        $scrolledWidth,
+        $scrolledHeight
+    );
+
+    /**
+     * 2. 画面の切り替えを通知
+     * Notify screen switching.
+     * 
+     * @param RemoteWebDriver $driver
+     * @param int $contentsWidth  実際のコンテンツ横幅
+     * @param int $contentsHeight 実際のコンテンツ縦幅
+     * @param int $scrolledWidth  現在スクロール済みの横幅
+     * @param int $scrolledHeight 現在スクロール済みの縦幅
+     */
+    public function notifyScreenSwitching(
+        RemoteWebDriver $driver,
+        $contentsWidth,
+        $contentsHeight,
+        $scrolledWidth,
+        $scrolledHeight
+    );
+
+    /**
+     * 3. 横幅が最初に末端まで到達したことを通知
+     * Notify that the view width has reached the end for the first.
+     * 
+     * @param RemoteWebDriver $driver
+     * @param int $contentsWidth  実際のコンテンツ横幅
+     * @param int $contentsHeight 実際のコンテンツ縦幅
+     * @param int $scrolledWidth  現在スクロール済みの横幅
+     * @param int $scrolledHeight 現在スクロール済みの縦幅
+     */
+    public function notifyThatViewWidthHasReachedEndForFirst(
+        RemoteWebDriver $driver,
+        $contentsWidth,
+        $contentsHeight,
+        $scrolledWidth,
+        $scrolledHeight
+    );
+
+    /**
+     * 4. 最初の縦スクロールを通知
+     * Notify first vertical scroll.
+     * 
+     * @param RemoteWebDriver $driver
+     * @param int $contentsWidth  実際のコンテンツ横幅
+     * @param int $contentsHeight 実際のコンテンツ縦幅
+     * @param int $scrolledWidth  現在スクロール済みの横幅
+     * @param int $scrolledHeight 現在スクロール済みの縦幅
+     */
+    public function notifyFirstVerticalScroll(
+        RemoteWebDriver $driver,
+        $contentsWidth,
+        $contentsHeight,
+        $scrolledWidth,
+        $scrolledHeight
+    );
+
+    /**
+     * 5. 縦幅が最初に末端まで到達したことを通知
+     * Notify that the view height has reached the end for the first.
+     * 
+     * @param RemoteWebDriver $driver
+     * @param int $contentsWidth  実際のコンテンツ横幅
+     * @param int $contentsHeight 実際のコンテンツ縦幅
+     * @param int $scrolledWidth  現在スクロール済みの横幅
+     * @param int $scrolledHeight 現在スクロール済みの縦幅
+     */
+    public function notifyThatViewHeightHasReachedEndForFirst(
+        RemoteWebDriver $driver,
+        $contentsWidth,
+        $contentsHeight,
+        $scrolledWidth,
+        $scrolledHeight
+    );
+
+    /**
+     * 6. 最後の画面が描画されることを通知
+     * Notify that the last view is drawn.
+     * 
+     * @param RemoteWebDriver $driver
+     * @param int $contentsWidth  実際のコンテンツ横幅
+     * @param int $contentsHeight 実際のコンテンツ縦幅
+     * @param int $scrolledWidth  現在スクロール済みの横幅
+     * @param int $scrolledHeight 現在スクロール済みの縦幅
+     */
+    public function notifyLastRender(
+        RemoteWebDriver $driver,
+        $contentsWidth,
+        $contentsHeight,
+        $scrolledWidth,
+        $scrolledHeight
+    );
+}
---
+++ sample/sample_7_controll_display_state_of_elements.php
@@ -0,0 +1,145 @@
+<?php
+
+require_once realpath(__DIR__ . '/../vendor') . '/autoload.php';
+
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\Remote\WebDriverBrowserType;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\WebDriverBy;
+use Facebook\WebDriver\WebDriverDimension;
+use Facebook\WebDriver\WebDriverExpectedCondition;
+use Facebook\WebDriver\Chrome;
+use Facebook\WebDriver\Firefox;
+
+use SMB\PhpWebDriver\Modules\Screenshot;
+use SMB\PhpWebDriver\Modules\View\Observer;
+
+/**
+ * selenium php-webdriver 要素の表示状態のコントロール サンプル
+ * @param string $browser chrome or firefox or ie
+ * @param array $size ['w' => xxx, 'h' => xxx]
+ * @param string overrideUA true : override Useragent
+ */
+function sample_7($browser, array $size=[], $overrideUA = '')
+{
+    // selenium
+    $host = 'http://localhost:4444/wd/hub';
+
+    switch ($browser) {
+        case WebDriverBrowserType::CHROME :
+            $cap = DesiredCapabilities::chrome();
+
+            if ($overrideUA !== '') {
+                $options = new Chrome\ChromeOptions();
+                $options->addArguments(['--user-agent=' . $overrideUA]);
+
+                $cap->setCapability(Chrome\ChromeOptions::CAPABILITY, $options);
+            }
+
+            if (getenv('CHROME_DRIVER_PATH') !== '') {
+                putenv('webdriver.chrome.driver=' . getenv('CHROME_DRIVER_PATH'));
+            }
+
+            $driver = RemoteWebDriver::create($host, $cap);
+
+            break;
+        case WebDriverBrowserType::FIREFOX :
+            $cap = DesiredCapabilities::firefox();
+
+            if ($overrideUA !== '') {
+                $profile = new Firefox\FirefoxProfile();
+                $profile->setPreference('general.useragent.override', $overrideUA);
+
+                $cap->setCapability(Firefox\FirefoxDriver::PROFILE, $profile);
+            }
+
+            if (getenv('FIREFOX_DRIVER_PATH') !== '') {
+                putenv('webdriver.gecko.driver=' . getenv('FIREFOX_DRIVER_PATH'));
+            }
+
+            $driver = RemoteWebDriver::create($host, $cap);
+
+            break;
+        case WebDriverBrowserType::IE :
+            if (getenv('IE_DRIVER_PATH') !== '') {
+                putenv('webdriver.ie.driver=' . getenv('IE_DRIVER_PATH'));
+            }
+            $driver = RemoteWebDriver::create($host, DesiredCapabilities::internetExplorer());
+            break;
+    }
+
+    // 画面サイズをMAXにする場合
+    // $driver->manage()->window()->maximize();
+
+    // 画面サイズの指定あり
+    if (isset($size['w']) && isset($size['h'])) {
+        $dimension = new WebDriverDimension($size['w'], $size['h']);
+        $driver->manage()->window()->setSize($dimension);
+    }
+
+    // 指定URLへ遷移 (Google)
+    $driver->get('https://www.google.co.jp/');
+
+    // 検索Box
+    $element = $driver->findElement(WebDriverBy::name('q'));
+    // 検索Boxにキーワードを入力して
+    $element->sendKeys('夏休みの予定');
+    // 検索実行
+    $element->submit();
+
+    // 指定した要素がコンテンツに出現するまで待ちます.
+    // #botstuff をターゲットにします.
+    $driver->wait(10)->until(
+        WebDriverExpectedCondition::visibilityOfElementLocated(WebDriverBy::id('botstuff'))
+    );
+
+    // キャプチャ
+    $fileName = $overrideUA === '' ? __METHOD__ . "_{$browser}.png" : __METHOD__ . "_override_ua_{$browser}.png";
+    $captureDirectoryPath = realpath(__DIR__ . '/../capture') . '/';
+
+    // オブザーバー
+    $observer = new Observer();
+    // 最初に縦スクロールが行われたら追従するヘッダー(スティッキーヘッダー)を消す
+    $observer->processForFirstVerticalScroll(function($driver) {
+        $driver->executeScript("document.querySelector('#searchform') ? document.querySelector('#searchform').style.display = 'none' : null;");
+    });
+
+    $screenshot = new Screenshot();
+
+    // オブザーバーをセット
+    $screenshot->setObserver($observer);
+
+    $screenshot->takeFull($driver, $captureDirectoryPath, $fileName);
+
+    // ブラウザを閉じる
+    $driver->close();
+}
+
+// iPhone6のサイズ
+$size4iPhone6 = ['w' => 375, 'h' => 667];
+
+// iOS12のUA
+$ua4iOS = 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Mobile/15E148 Safari/604.1';
+
+/**
+ |------------------------------------------------------------------------------
+ | 有効にしたいドライバーの値を true にしてください
+ |------------------------------------------------------------------------------
+ */
+
+// chrome
+if (getenv('ENABLED_CHROME_DRIVER') === 'true') {
+    sample_7(WebDriverBrowserType::CHROME);
+    sample_7(WebDriverBrowserType::CHROME, $size4iPhone6, $ua4iOS);
+}
+
+// firefox
+if (getenv('ENABLED_FIREFOX_DRIVER') === 'true') {
+    sample_7(WebDriverBrowserType::FIREFOX);
+    sample_7(WebDriverBrowserType::FIREFOX, $size4iPhone6, $ua4iOS);
+}
+
+// ie
+if (getenv('ENABLED_IE_DRIVER') === 'true') {
+    sample_7(WebDriverBrowserType::IE);
+}
